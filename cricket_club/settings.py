d197f4f18df830708b30fff149326c92cfe1555a
shubhamsingh851 <shus@americana-food.com>
2026-02-23 17:25:15 +0530

    Image on docker

--- cricket_club/settings.py
+++ cricket_club/settings.py
@@ -1,14 +1,8 @@
 """
 Django settings for cricket_club project.
 """
-import pymysql
 from pathlib import Path
 import os
-
-
-pymysql.version_info = (1, 4, 6, 'final', 0)
-pymysql.install_as_MySQLdb()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
