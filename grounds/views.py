0f6b5ed576c8ac256ca7402628d69d046899600f
shubhamsingh851 <shus@americana-food.com>
2026-02-27 15:15:01 +0530

    public

--- grounds/views.py
+++ grounds/views.py
@@ -1,7 +1,13 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import Ground
 from .serializers import GroundSerializer
 
 class GroundViewSet(viewsets.ModelViewSet):
     queryset = Ground.objects.all()
     serializer_class = GroundSerializer
+
+    def get_permissions(self):
+        if self.action in ("list", "retrieve"):
+            return [AllowAny()]
+        return [IsAuthenticated()]
